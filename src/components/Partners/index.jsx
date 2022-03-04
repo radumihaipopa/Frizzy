46273f57d3a92bce5d5a7d33c06010ba753e42e8
Mihai <radu_mihai.popa@yahoo.com>
2022-03-04 11:39:47 +0200

    Modified initial scale

--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import {ServicesContainer, ServicesH1, ServicesWrapper, ServicesCard, ServicesIcon1, ServicesIcon2} from './ServiceElements'
+import {ServicesContainer, ServicesH1, ServicesWrapper, ServicesCard, ServicesIcon1, ServicesIcon2, ServicesIcon3, ServicesH2} from './ServiceElements'
 
 const Partners = () => {
   return (
     <ServicesContainer id='partners'>
       <ServicesH1>Partners</ServicesH1>
       <ServicesWrapper>
+          <ServicesCard href='//elrondgiants.com/'>
+              <ServicesIcon3 src={'/images/giants.jpeg'}/>
+              <ServicesH2 src={'/images/giants.png'}/>
+          </ServicesCard>
           <ServicesCard href='//karmagurunft.com/'>
               <ServicesIcon2 src={'/images/guru.png'}/>
           </ServicesCard>
